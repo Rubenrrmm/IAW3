edf16806644c6157a5123648ab71e9affcbd334b
Rubenrrmm <146704184+Rubenrrmm@users.noreply.github.com>
2023-10-09 09:48:23 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+          echo "<div>Encantado de conocerte, $nombre";
+        ?>
+    </body>
+</html>
